cb87c2cf29544ffa09d68f51edd8087df5b63629
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2023-03-02 18:33:31 -0500

    Finished the styling for the SearchPage and the VideoGameDetailPage.

--- video-game-data-frontend/src/pages/VideoGameDetailPage/VideoGameDetailPage.jsx
+++ video-game-data-frontend/src/pages/VideoGameDetailPage/VideoGameDetailPage.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import axios from 'axios'
 import { useState, useEffect } from "react";
+import Container from 'react-bootstrap/Container'
+import './VideoGameDetailPage.css'
 
 const VideoGameDetailPage = ({}) => {
     const { gameId } = useParams();
@@ -19,18 +21,22 @@ const VideoGameDetailPage = ({}) => {
     console.log(game)
     return (
         game && (
-            <div>
-                <h1>Game Title: {game.name}</h1>
-                <h2>Publuisher: {game.publisher}</h2>
-                <h2>Platform: {game.game_rank}</h2>
-                <h2>Year: {game.year}</h2>
-                <h3>Game Rank: {game.game_rank}</h3>
-                <h3>Global Sales: {game.globalsales} Million</h3>
-                <h3>Genre: {game.genre}</h3>
-                <h4>North America Sales: {game.northamericasales} Million</h4>
-                <h4>Europe Sales: {game.europesales} Million</h4>
-                <h4>Japan Sales: {game.japansales} Million</h4>
-                <h4>Other Sales: {game.othersales} Million</h4>
+            <div className="centered">
+                <Container>
+                        <h1 className="game-title">Game Title: {game.name}</h1>
+                    <Container className="border-box centered">
+                        <h1>Publisher: {game.publisher}</h1>
+                        <h1>Platform: {game.platform}</h1>
+                        <h2>Year: {game.year}</h2>
+                        <h2>Game Rank: {game.game_rank}</h2>
+                        <h2>Global Sales: {game.globalsales} Million</h2>
+                        <h3>Genre: {game.genre}</h3>
+                        <h3>North America Sales: {game.northamericasales} Million</h3>
+                        <h3>Europe Sales: {game.europesales} Million</h3>
+                        <h3>Japan Sales: {game.japansales} Million</h3>
+                        <h3>Other Sales: {game.othersales} Million</h3>
+                    </Container>
+                </Container>
             </div>
         )
     );
